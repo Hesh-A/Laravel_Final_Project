cb12e7269b7382d87a6cb47314ca695b516f2f26
Arshana <arshanaheshanekanayake.ekanayakemudiyanselage@griffithuni.edu.au>
2026-07-28 17:45:58 +1000

    [FIX][UI] forward extra attributes to field component input via attributes bag

--- resources/views/components/forms/field.blade.php
+++ resources/views/components/forms/field.blade.php
@@ -8,7 +8,7 @@
 
 <div class="space-y-2">
          <label for="{{ $name }}" class="label"> {{  $label }} </label>
-         <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" class="input" value="{{ old($name) }}" /> 
+         <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" class="input" value="{{ old($name) }}" {{ $attributes }} /> 
 
          @error($name)
             <p class="error"> {{ $message }} </p>
