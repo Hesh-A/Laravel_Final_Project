35ee8c1df2fb373e3b58e01886ec28cd1bf639d6
Arshana <arshanaheshanekanayake.ekanayakemudiyanselage@griffithuni.edu.au>
2026-07-29 10:59:13 +1000

    [TEST][AUTH] update browser login flows

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -12,8 +12,33 @@ it('logs in a user', function () {
     visit('/login')
         ->fill('email', 'johndoe@mail.com')
         ->fill('password', 'password12345')
-        ->click('[type="submit"]')
+        ->click('@login-button')
         ->assertPathIs('/');
 });
 
 
+it ('logs out a user', function () {
+    //when a user exists in the database
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+  
+    // the logout works
+    visit('/')
+        ->click('@logout-button')
+        ->assertPathIs('/login');
+
+    
+    $this->assertGuest();
+    
+});
+
+it('handles invalid login', function () {
+    visit('/login')
+        ->fill('email', ' ')
+        ->fill('password', ' ')
+        ->click('@login-button')
+        
+        ->assertPathIs('/login');
+    
+});
--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -1,12 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+
 it('registers a user', function () {
     visit('/register')
         ->fill('name', 'Johas ')
         ->fill('email', 'johndoe@mail.com')
         ->fill('password', 'password12345')
-        ->click('[type="submit"]')
+        ->click('@register-button')
         ->assertPathIs('/');
+
+    $this->assertAuthenticated();
+
+    expect(Auth::user())->toMatchArray([
+        'name' => 'Johas',
+        'email' => 'johndoe@mail.com',
+    ]);
 });
 
 
