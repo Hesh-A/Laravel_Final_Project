168dbbabcec5f4a6e459ea4d2c34b2c067ce6c1c
Arshana <arshanaheshanekanayake.ekanayakemudiyanselage@griffithuni.edu.au>
2026-07-30 15:55:59 +1000

    [TEST][IDEAS] cover idea creation with links

--- tests/Browser/CreateIdeaTest.php
+++ tests/Browser/CreateIdeaTest.php
@@ -14,10 +14,19 @@ it('creates an idea', function () {
 		->fill('title', 'Build something')
         ->click('@status-button-pending')
 		->fill('description', 'Create happy path.')
+		->fill('@new-link', 'https://www.example.com')
+		->click('@add-new-link-button')
+		->fill('@new-link', 'https://www.laravel.com')
+		->click('@add-new-link-button')
 		->click('@store-idea-button')
 		->assertPathIs('/ideas');
 
-	expect(Idea::query()->where('title', 'Build something')->exists())->toBeTrue();
+	expect($user->ideas()->first())->toMatchArray([
+		'title' => 'Build something',
+		'description' => 'Create happy path.',
+		'status' => 'pending',
+		'links' => ['https://www.example.com', 'https://www.laravel.com'],
+	]);
 });
 
 it('handles invalid idea creation', function () {
